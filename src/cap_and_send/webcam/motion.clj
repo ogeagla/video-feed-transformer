850cf82ead6db8e9fb21e4fd2166a9014da92fbe
Octavian Geagla <ogeagla@gmail.com>
2016-11-16 16:39:05 -0700

    motion file params now are changed

--- src/cap_and_send/webcam/motion.clj
+++ src/cap_and_send/webcam/motion.clj
@@ -14,18 +14,28 @@
   newfile)
 
 (defn- alter-conf [file motion-dir device]
-  ())
+  (try
+    (let [out1 (sh "sed" "-i" "-e" (str "s#motion-dir#" motion-dir "#g") file)
+          out2 (sh "sed" "-i" "-e" (str "s#/dev/video0#" device "#g") file)]
+      (if-not (and (= (:exit out1) 0) (= (:exit out2) 0))
+        (println "ERROR sed did not alter motion config file: " out1 "\n" out2)))
+    (catch Throwable t
+      (println "ERROR could not alter motion config file " t))))
 
 (defn- do-motion-conf [config-dest motion-dir device] ""
+  (println "INFO making motion config file: " config-dest
+           "\n with params: motion-dir: " motion-dir "\n device: " device)
   (copy-conf-to-new config-dest)
-  (alter-conf config-dest motion-dir device))
+  (alter-conf config-dest motion-dir device)
+  config-dest)
 
 (defn- do-motion [motion-dir device] ""
   (println "INFO running motion with output dir: " motion-dir " and device: " device)
-  ;TODO actually use motion-dir and device
-  (let [in ["motion" "-n" "-c" "resources/motion.conf"]]
+  (let [conf-to-use "resources/motion-0.conf"
+        in ["motion" "-n" "-c" "resources/motion.conf"]]
     (try
       (println "INFO motion sh input: " in)
+      (do-motion-conf conf-to-use motion-dir device)
       (apply sh in)
       (catch Throwable t
         (println "ERROR motion error: " (:cause (Throwable->map t)))))))
