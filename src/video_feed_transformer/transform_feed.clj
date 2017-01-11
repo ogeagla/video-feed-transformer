f611ad23c1ecac76adbce0a4507fdcb7865d91e4
Octavian Geagla <ogeagla@gmail.com>
2017-01-11 16:23:56 -0700

    [style] cleanup unused code

--- src/video_feed_transformer/transform_feed.clj
+++ src/video_feed_transformer/transform_feed.clj
@@ -101,7 +101,6 @@
   (let [combo (imgz/copy background-img)
         g     (.getGraphics combo)]
     (doseq [{:keys [img x y]} foreground-imgs-and-coordinates]
-      (println "drawing img to x, y: " x y)
       (.drawImage g img x y nil))
     (.dispose g)
     combo))
@@ -147,12 +146,11 @@
 
         target-w                                (.getWidth target-img)
         target-h                                (.getHeight target-img)
-        _                                       (println "canvas w, h: " target-w, target-h)
         blank-canvas                            (imgz/new-image target-w target-h)
         final                                   (overlay-many blank-canvas stuff-ready-for-composing)
         _                                       (imgz/save final "test-final-img.png")
         ]
-    (println "STUFF2 " stuff-ready-for-composing)))
+    final))
 
 (defn- move-file [src-file dest-dir] ""
   (fs/copy+ src-file dest-dir))
@@ -168,37 +166,8 @@
 
 (def feed-chan (chan))
 
-(defn- do-feed-from-frames [fps frame-dir clip-intermediate-dir clipno s3-bucket feed-dir] ""
-  (let [the-frames         (fs/list-dir frame-dir)
-        the-frames-ordered (sort-by #(get-clip-number %) the-frames)
-        with-abs           (map-indexed (fn [idx itm]
-                                          (hash-map :idx idx
-                                                    :file itm
-                                                    :new-file (str clip-intermediate-dir "/" (format "%06d" idx) ".jpeg")))
-                                        the-frames-ordered)]
-    (do
-      (println "INFO making clip from frame count: " (count the-frames))
-      (doseq [f with-abs]
-        (move-file (:file f) (:new-file f)))
-      (let [the-new-frames (fs/list-dir clip-intermediate-dir)
-            in             ["ffmpeg" "-r"
-                            (str fps)
-                            "-f" "image2" "-s" "1920x1080" "-i"
-                            (str clip-intermediate-dir "/%6d.jpeg")
-                            "-vcodec" "libx264" "-crf" "25" "-pix_fmt" "yuv420p"
-                            (str feed-dir "/" clipno ".mp4")]]
-        (try
-          (println "INFO clip input: " in)
-          (apply sh in)
-          (catch Throwable t
-            (println "ERROR clip error: " (:cause (Throwable->map t)))))
-        (doseq [f the-new-frames]
-          (delete-file f))
-        (doseq [f the-frames]
-          (delete-file f))
-        [{:file   clipno
-          :bucket s3-bucket}]))))
-
+(defn- do-feed-from-frames [frame-dir s3-bucket] ""
+  (let [the-frames         (fs/list-dir frame-dir)]))
 
 (defn- do-feed [fps frame-dir clip-intermediate-dir clipno s3-bucket s3-dir s3-upload-chan
                 motion-dir use-motion upload-to-s3 motion-summary-dir feed-dir]
@@ -206,7 +175,7 @@
   ; s3-bucket s3-dir s3-upload-chan motion-dir use-motion)
   (let [clips-data (if use-motion
                      (println "ERROR no support for transforming motion clips as feeds")
-                     (do-feed-from-frames fps frame-dir clip-intermediate-dir clipno s3-bucket feed-dir))]
+                     (do-feed-from-frames frame-dir s3-bucket))]
     (if upload-to-s3
       (doseq [datum clips-data]
         (println "INFO putting to s3: " datum)
